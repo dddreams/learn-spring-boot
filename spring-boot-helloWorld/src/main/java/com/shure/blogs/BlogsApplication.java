9d0ee0ed496590ae1e0044e4b051cd93ed6e2c4e
dddreams <dd_dreams@163.com>
2018-06-29 19:07:51 +0800

    spring boot hello world

---
+++ spring-boot-helloWorld/src/main/java/com/shure/blogs/BlogsApplication.java
@@ -0,0 +1,12 @@
+package com.shure.blogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsApplication.class, args);
+	}
+}
---
+++ spring-boot-helloWorld/src/main/java/com/shure/blogs/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.shure.blogs.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String hello(){
+        return "hello world!";
+    }
+
+}
