cf70c07c9d945d01fd3115d6a2aee78492c13eb6
shure <dd_dreams@163.com>
2021-02-22 22:46:54 +0800

    jdbc多数据源代码示例

---
+++ spring-boot-jdbc-multiple/src/main/java/com/shure/jdbc/SpringBootJdbcMultipleApplication.java
@@ -0,0 +1,13 @@
+package com.shure.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcMultipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcMultipleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-jdbc-multiple/src/test/java/com/shure/jdbc/SpringBootJdbcMultipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shure.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcMultipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
